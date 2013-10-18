5d5e204c156d1aa786443b3258054638d48f9d14
Xynnect <dido.jordanov7@gmail.com>
2013-10-18 14:51:57 +0100

    Update LogData.java

--- src/com/example/y4/LogData.java
+++ src/com/example/y4/LogData.java
@@ -2,22 +2,26 @@ package com.example.y4;
 
 import java.util.LinkedList;
 
+import android.util.Log;
 import android.widget.TextView;
 
 public class LogData {
 	String logData = "";
-	LinkedList<TextView> views = new LinkedList<TextView>();
+	static LinkedList<TextView> views = new LinkedList<TextView>();
 	public LogData(){
 	}
 	public void registerView(TextView view){
+		Log.v("dd","onStart");
 		views.add(view);
 		logData = (String) view.getText();
 	}
 	public void addLine(String line){
+		Log.v("dd","onStart");
 		logData.concat(line);
 		
 	}
 	public void removeView(TextView view){
+		Log.v("dd","onStart");
 		views.remove(view);
 		
 	}
